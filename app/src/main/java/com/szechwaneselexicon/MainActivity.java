1d6750fb3b807d880a49992950aa1cf2dd5138f1
Harold Lee <zttofficial@users.noreply.github.com>
2020-08-19 02:47:54 +0800

    Frame

---
+++ app/src/main/java/com/szechwaneselexicon/MainActivity.java
@@ -0,0 +1,21 @@
+package com.szechwaneselexicon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void retrieve(View view) {
+        // 寫要做的事...
+    }
+}
