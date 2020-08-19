8fc284b19829c5ed5cfe79f4bd608cd8c73da0be
Harold Lee <zttofficial@users.noreply.github.com>
2020-08-20 00:23:24 +0800

    Achieve switch activeties

--- app/src/main/java/com/szechwaneselexicon/MainActivity.java
+++ app/src/main/java/com/szechwaneselexicon/MainActivity.java
@@ -2,8 +2,10 @@ package com.szechwaneselexicon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button retrieve = (Button) findViewById(R.id.retrieve);
+
+        retrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Result.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void retrieve(View view) {
-        // 寫要做的事...
-    }
+
 }
---
+++ app/src/main/java/com/szechwaneselexicon/Result.java
@@ -0,0 +1,14 @@
+package com.szechwaneselexicon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
