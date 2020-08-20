5bf620dc720a43a109250b1fe1d18131eb4ebe61
Harold Lee <zttofficial@users.noreply.github.com>
2020-08-21 00:56:22 +0800

    Basic function version

--- app/src/main/java/com/szechwaneselexicon/Result.java
+++ app/src/main/java/com/szechwaneselexicon/Result.java
@@ -22,19 +22,25 @@ public class Result extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView result = findViewById(R.id.textView);
         TextView Paraphrase = findViewById(R.id.textView2);
+        TextView Tone = findViewById(R.id.textView3);
 
 
         SQLite s = new SQLite();
         SQLiteDatabase db =s.openDatabase(getApplicationContext());
-        Cursor cursor = db.rawQuery("select * from lexicon where character=?", new String[]{message});
+        Cursor cursor = db.rawQuery("select * from lexicon where character=?", new String[] {message});
         String paraphraseResult = null;
-        if(cursor.moveToFirst()){
+        String toneResult = null;
+        while(cursor.moveToNext()){
             paraphraseResult = cursor.getString(cursor.getColumnIndex("paraphrase"));
+            toneResult = cursor.getString(cursor.getColumnIndex("tone"));
         }
+
         result.setText(message);
         Paraphrase.setText(paraphraseResult);
+        Tone.setText(toneResult);
         //System.out.print(paraphraseResult);
         //System.out.print(message);
         cursor.close();
+        db.close();
     }
 }
